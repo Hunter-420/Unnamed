fc6cb9bc516a4ddd5c4154386408ae9a7adb9b6e
Nischal Khanal <khanalnischal2075@gmail.com>
2024-07-03 17:01:09 +0545

    fix: cores issue

--- server/server.js
+++ server/server.js
@@ -8,8 +8,9 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
-app.use(express.json());
+server.use(express.json());
+server.use(cors());
+server.options('*', cors());
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
