d07a8d86a2057e5f3b373b5665a219ab5325e321
Nischal Khanal <khanalnischal2075@gmail.com>
2024-07-04 16:54:45 +0545

    test: vercel cores simplified server

--- server/server.js
+++ server/server.js
@@ -9,22 +9,16 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Use CORS with specific configuration
-const corsOptions = {
-  origin: 'https://unnamed-two.vercel.app',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
-};
-
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions)); // Pre-flight requests
-
 app.use(express.json());
+app.use(cors());
+app.options('*', cors());
+
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true, // Optional if using latest Mongoose versions
-  useUnifiedTopology: true // Optional if using latest Mongoose versions
+  autoIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 })
 .then(() => {
   console.log('Connected to MongoDB');
