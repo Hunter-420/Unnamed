38c5c2e3c66df0e1adf6dc29f0fe34bd168474f8
Nischal Khanal <khanalnischal2075@gmail.com>
2024-07-04 13:44:28 +0545

    test: vercel cores

--- server/server.js
+++ server/server.js
@@ -8,20 +8,15 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Use cors with all origins
-app.use(cors());
-// Use cors with specific configuration
-const corsOptions = {
-  origin: '*', // Allows all origins
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  preflightContinue: false,
-  optionsSuccessStatus: 204,
-};
+// CORS configuration
+app.use(cors({
+  origin: 'https://unnamed-two.vercel.app', // Your React app's URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
 
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions)); // Pre-flight requests
 app.use(express.json());
-
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
@@ -40,8 +35,9 @@ app.use('/api', authRoutes); // Authentication routes
 const productRoutes = require('./routes/productRoutes');
 app.use('/api', productRoutes); // Product routes
 
+// Handle OPTIONS requests for preflight
+app.options('*', cors());
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
-
-
