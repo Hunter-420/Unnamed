47d191dc38835f62ea3e67f6e3d292d5251cd219
Nischal Khanal <khanalnischal2075@gmail.com>
2024-07-04 16:35:21 +0545

    test: vercel cores simplified server

--- server/server.js
+++ server/server.js
@@ -8,11 +8,9 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Use cors with all origins
-app.use(cors());
-// Use cors with specific configuration
+// Use CORS with specific configuration
 const corsOptions = {
-  origin: 'https://unnamed-two.vercel.app', // Allows specific origin
+  origin: 'https://unnamed-two.vercel.app',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
@@ -20,14 +18,18 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions)); // Pre-flight requests
+
 app.use(express.json());
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
-  // Deprecated options removed
-}).then(() => {
+  useNewUrlParser: true, // Optional if using latest Mongoose versions
+  useUnifiedTopology: true // Optional if using latest Mongoose versions
+})
+.then(() => {
   console.log('Connected to MongoDB');
-}).catch(err => {
+})
+.catch(err => {
   console.error('Error connecting to MongoDB', err);
 });
 
@@ -41,5 +43,3 @@ app.use('/api', productRoutes); // Product routes
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
-
-module.exports = app;
