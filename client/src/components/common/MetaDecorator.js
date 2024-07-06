c5247ca3cee641d2b12059c952aa33d5b6200cfd
Nischal Khanal <khanalnischal2075@gmail.com>
2024-07-06 23:52:41 +0545

    feat: preview sharing of product implementing metadecorator

--- client/src/components/common/MetaDecorator.js
+++ client/src/components/common/MetaDecorator.js
@@ -4,10 +4,10 @@ import { Helmet } from "react-helmet";
 function MetaDecorator({ title = "Default Title", description = "Default Description", src, url }) {
     return (
         <Helmet>
-            <meta charSet="utf-8" />
+
+            <meta property="og:image" content={src} />
             <meta property="og:title" content={title} />
             <meta property="og:description" content={description} />
-            <meta property="og:image" content={src} />
             <meta property="og:url" content={url} />
             <meta name='facebook:card' content='summary_large_image' />
             <meta name='instagram:card' content='summary_large_image' />
